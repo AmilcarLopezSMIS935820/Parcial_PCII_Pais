11111e4806f6c2cea9b956a826f79d285e26e14b
SrWyatt <esau_batres2001@hotmail.com>
2021-04-30 22:30:40 -0500

    SE actualizaron los métodos | Josué Guardado, SMIS058820

--- app/src/main/java/com/smis935820/parcial_pcii/MainActivity.java
+++ app/src/main/java/com/smis935820/parcial_pcii/MainActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btmain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btmain = findViewById(R.id.button);
+        btmain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,Registros.class);
+            }
+        });
     }
 
     public void ini(View view){
-        Intent i = new Intent(this, Registros.class);
+        Intent i = new Intent(MainActivity.this, formulario.class);
         startActivity(i);
         finish();
+
+
+
+
     }
 }
--- app/src/main/java/com/smis935820/parcial_pcii/formulario.java
+++ app/src/main/java/com/smis935820/parcial_pcii/formulario.java
@@ -2,6 +2,7 @@ package com.smis935820.parcial_pcii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.smis935820.parcial_pcii.interfaces.paisAPI;
+import com.smis935820.parcial_pcii.models.Pais;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class formulario extends AppCompatActivity {
     EditText etid, etname, etdescripcion;
@@ -30,6 +36,9 @@ public class formulario extends AppCompatActivity {
         tvResponse = findViewById(R.id.tvResponse);
 
 
+
+
+
         btnsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,9 +46,76 @@ public class formulario extends AppCompatActivity {
                     Toast.makeText(formulario.this, "Los campos no deben quedar vacios", Toast.LENGTH_SHORT).show();
                     return;
 
-                    //Llamado al metodo POST
+                    //Llamado al metodo
+
                 }
+                savePais(etname.getText().toString(), etdescripcion.getText().toString());
+            }
+        });
+
+        //actualizar
+        btnedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editPais(etid.getText().toString(),etname.getText().toString(), etdescripcion.getText().toString());
+
             }
         });
     }
+
+
+    public void savePais(String nombre, String descripcion){
+        //llamando interfaz
+        Pais pais = new Pais(nombre, descripcion);
+        paisAPI.savePais(pais).enqueue(new Callback<Pais>() {
+            @Override
+            public void onResponse(Call<Pais> call, Response<Pais> response) {
+                Pais responsePais = response.body();
+                String responseString = "Response code: " + response.code() +
+                        "\nData: "+ pais.toString();
+
+                //mostrar respuesta
+                showResponse(responseString.toString());
+
+            }
+
+            @Override
+            public void onFailure(Call<Pais> call, Throwable t) {
+                showResponse(t.getMessage());
+
+            }
+        });
+
+    }
+    public void editPais(String id, String nombre, String descripcion){
+        //llamando interfaz
+        Pais pais = new Pais(nombre, descripcion);
+        paisAPI.updatePais(pais).enqueue(new Callback<Pais>() {
+            @Override
+            public void onResponse(Call<Pais> call, Response<Pais> response) {
+                Pais responsePais = response.body();
+                String responseString = "Response code: " + response.code() +
+                        "\nData: "+ pais.toString();
+
+                //mostrar respuesta
+                showResponse(responseString.toString());
+
+            }
+
+            @Override
+            public void onFailure(Call<Pais> call, Throwable t) {
+                showResponse(t.getMessage());
+
+            }
+        });
+
+    }
+    // hacer visible el textview-tvResponse
+    public void showResponse(String response){
+        if (tvResponse.getVisibility()==View.GONE){
+            tvResponse.setVisibility(View.VISIBLE);
+        }
+        tvResponse.setText(response);
+    }
+
 }
